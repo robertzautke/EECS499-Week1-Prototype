ecffe42d3a1434b57b14720e6829a8b685312e1a
rzautke <rzautke@umich.edu>
2015-01-15 18:32:16 -0500

    Completely changed everything...

--- Assets/Week 1 Prototype/UI_Functions.cs
+++ Assets/Week 1 Prototype/UI_Functions.cs
@@ -5,41 +5,20 @@ using UnityEngine.UI;
 public class UI_Functions : MonoBehaviour {
 
     public GameObject canvas_envelope;
-    public GameObject canvas2_envelope;
-    public GameObject local_canvas_envelope;
     public InputField inputField;
     public Button clientConnect;
     public Button startServer;
-    public Button add;
-    public Text countText;
 
-    private int count = 0;
-    private bool serverAlreadyStarted = false;
-    private bool alreadyConnected = false;
-
-	void Start () {
-        
-	}
-	
-	void Update () {
-	
-	}
+    public bool serverAlreadyStarted = false;
+    public bool alreadyConnected = false;
+    private int numberOfPlayers = 0;
+	public int numberOfPlayersNeeded = 1;
 
     public void startServerButtonClick() {
         if (!serverAlreadyStarted) {
             Network.incomingPassword = "password";
             bool useNat = !Network.HavePublicAddress();
             Network.InitializeServer(32, 25000, useNat);
-            Destroy(canvas_envelope);
-            Network.Instantiate(canvas2_envelope, transform.position, transform.rotation, 0);
-
-            add = GameObject.Find("Add").GetComponent<Button>();
-            add.onClick.AddListener(() => { addToCounter(); });
-            print(add.GetComponentInChildren<Text>().text);
-            countText = GameObject.Find("Count").GetComponent<Text>();
-            print(countText.text);
-            count = 0;
-            serverAlreadyStarted = true;
         }    
     }
 
@@ -47,44 +26,41 @@ public class UI_Functions : MonoBehaviour {
         if (!alreadyConnected) {
 
             ConnectToServer();
-            Destroy(canvas_envelope);
 
         }
     } 
-
-
-    public void addToCounter() {
-        networkView.RPC("addToAllCounters", RPCMode.All);
-    }
-
-    [RPC]
-    public void addToAllCounters() {
-        count++;
-        countText.text = count.ToString();
-    }
 
     void ConnectToServer()
     {
         Network.Connect(inputField.text.ToString(), 25000, "password");
     }
 
-////Generic debug output for server/client status////
-
-    private int playerCount = 0;
+////Generic events and debug output for server/client status////
 
     void OnServerInitialized()
     {
         Debug.Log("Server initialized and ready");
+
+        serverAlreadyStarted = true;
+        numberOfPlayers++;
+        Destroy(canvas_envelope);
+
+		if (numberOfPlayers == numberOfPlayersNeeded)
+		{
+			Debug.Log("Game is Starting");
+			StartGame();
+		}
     }
+
+
     void OnConnectedToServer()
     {
         Debug.Log("Connected to server");
         alreadyConnected = true;
-
-        add = GameObject.Find("addToCounter").GetComponent<Button>();
-        add.onClick.AddListener(() => { addToCounter(); });
-        countText = GameObject.Find("Count").GetComponent<Text>();
+		Destroy(canvas_envelope);
     }
+
+
     void OnDisconnectedFromServer(NetworkDisconnection info)
     {
         if (Network.isServer)
@@ -95,23 +71,76 @@ public class UI_Functions : MonoBehaviour {
             else
                 Debug.Log("Successfully diconnected from the server");
     }
+
+
     void OnFailedToConnect(NetworkConnectionError error)
     {
         Debug.Log("Could not connect to server: " + error);
         alreadyConnected = false;
     }
+
+
     void OnNetworkInstantiate(NetworkMessageInfo info)
     {
         Debug.Log("New object instantiated by " + info.sender);
+
+		Debug.Log(networkView.viewID + " spawned");
+		if (Network.isServer)
+		{
+			Network.RemoveRPCs(networkView.viewID);
+			Network.Destroy(gameObject);
+		}
     }
+
+
     void OnPlayerConnected(NetworkPlayer player)
     {
-        Debug.Log("Player " + playerCount++ + " connected from " + player.ipAddress + ":" + player.port);
+        numberOfPlayers++;
+        Debug.Log("Player " + numberOfPlayers + " connected from " + player.ipAddress + ":" + player.port);
+
+        if (numberOfPlayers == numberOfPlayersNeeded) {
+            Debug.Log("Game is Starting");
+			StartGame();
+        }
+
     }
+
+
     void OnPlayerDisconnected(NetworkPlayer player)
     {
         Debug.Log("Clean up after player " + player);
         Network.RemoveRPCs(player);
         Network.DestroyPlayerObjects(player);
+        numberOfPlayers--;
     }
+
+/////////////////////////////////////////////////////////////////////////////////
+
+	public GameObject back;
+	public GameObject cursor;
+	private GameObject scriptCursor;
+	private bool gameStarted = false;
+
+	Vector3 rayCollision;
+	Vector2 mousePos;
+
+	void StartGame() {
+
+		gameStarted = true;
+		back.renderer.enabled = true;
+		scriptCursor = (GameObject)Network.Instantiate(cursor, new Vector3(0, 0, -0.5f), transform.rotation, 0);
+	}
+
+	void Update() {
+		if (gameStarted) {
+
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			Physics.Raycast(ray, out hit);
+			Debug.Log("This hit at " + hit.point);
+
+			Vector3 c = new Vector3(hit.point.x, hit.point.y, -0.5f);
+			scriptCursor.transform.position = c;
+		}
+	}
 }
