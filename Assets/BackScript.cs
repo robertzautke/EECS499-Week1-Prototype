99d9169b6a41075af55ac52af7c655053f62ea8a
rzautke <rzautke@umich.edu>
2015-01-19 16:12:37 -0500

    no message

---
+++ Assets/BackScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackScript : MonoBehaviour {
+
+	[SerializeField]
+	public GameObject[] markerArray;
+
+	BoardSpace[] bs;
+	private class BoardSpace
+	{
+		public bool taken;
+		public int player;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		bs = new BoardSpace[9];
+		for (int i = 0; i < 9; i++) {
+			bs[i] = new BoardSpace();
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void GameWinConditionCheck(int i, int playerNum)
+	{
+
+		bs[i].taken = true;
+		bs[i].player = playerNum;
+
+		if (bs[0].taken && bs[0].player == 0 && bs[1].taken && bs[1].player == 0 && bs[2].taken && bs[2].player == 0 ||
+			bs[3].taken && bs[3].player == 0 && bs[4].taken && bs[4].player == 0 && bs[5].taken && bs[5].player == 0 ||
+			bs[6].taken && bs[6].player == 0 && bs[7].taken && bs[7].player == 0 && bs[8].taken && bs[8].player == 0 ||
+			bs[0].taken && bs[0].player == 0 && bs[3].taken && bs[3].player == 0 && bs[6].taken && bs[6].player == 0 ||
+			bs[1].taken && bs[1].player == 0 && bs[4].taken && bs[4].player == 0 && bs[7].taken && bs[7].player == 0 ||
+			bs[2].taken && bs[2].player == 0 && bs[5].taken && bs[5].player == 0 && bs[8].taken && bs[8].player == 0 ||
+			bs[0].taken && bs[0].player == 0 && bs[4].taken && bs[4].player == 0 && bs[8].taken && bs[8].player == 0 ||
+			bs[2].taken && bs[2].player == 0 && bs[4].taken && bs[4].player == 0 && bs[6].taken && bs[6].player == 0)
+		{
+
+			print("Player 1 wins!");
+
+		}
+		else if (bs[0].taken && bs[0].player == 1 && bs[1].taken && bs[1].player == 1 && bs[2].taken && bs[2].player == 1 ||
+		  bs[3].taken && bs[3].player == 1 && bs[4].taken && bs[4].player == 1 && bs[5].taken && bs[5].player == 1 ||
+		  bs[6].taken && bs[6].player == 1 && bs[7].taken && bs[7].player == 1 && bs[8].taken && bs[8].player == 1 ||
+		  bs[0].taken && bs[0].player == 1 && bs[3].taken && bs[3].player == 1 && bs[6].taken && bs[6].player == 1 ||
+		  bs[1].taken && bs[1].player == 1 && bs[4].taken && bs[4].player == 1 && bs[7].taken && bs[7].player == 1 ||
+		  bs[2].taken && bs[2].player == 1 && bs[5].taken && bs[5].player == 1 && bs[8].taken && bs[8].player == 1 ||
+		  bs[0].taken && bs[0].player == 1 && bs[4].taken && bs[4].player == 1 && bs[8].taken && bs[8].player == 1 ||
+		  bs[2].taken && bs[2].player == 1 && bs[4].taken && bs[4].player == 1 && bs[6].taken && bs[6].player == 1)
+		{
+
+			print("Player 2 wins!");
+
+		}
+	}
+}
---
+++ Assets/marker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class marker : MonoBehaviour {
+
+	public GameObject ui_controller;
+	public GameObject back;
+	public bool clickedServer = false;
+	public bool clickedClient = false;
+
+
+
+	void Start () {
+		ui_controller = GameObject.Find("UI_Controller");
+		back = this.transform.parent.gameObject;
+	}
+
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		if (ui_controller.GetComponent<UI_Functions>().serverAlreadyStarted &&
+			ui_controller.GetComponent<UI_Functions>().playerTurn == 0 &&
+			!clickedServer && !clickedClient) {
+			
+			ui_controller.GetComponent<UI_Functions>().playerTurn = 1;
+			clickedServer = true;
+			this.renderer.material.color = Color.blue;
+
+			string s = this.name;
+			char markerNumberChar = s[s.Length - 1];
+			double markerValueDouble = Char.GetNumericValue(markerNumberChar);
+			int markerNumber = Convert.ToInt32(markerValueDouble);
+			print(markerNumber);
+			back.GetComponent<BackScript>().GameWinConditionCheck(markerNumber, 0);
+		 }
+
+		else if (ui_controller.GetComponent<UI_Functions>().clientConnect &&
+			ui_controller.GetComponent<UI_Functions>().playerTurn == 1 &&
+			!clickedServer && !clickedClient)
+		{
+
+			ui_controller.GetComponent<UI_Functions>().playerTurn = 0;
+			clickedClient = true;
+			this.renderer.material.color = Color.red;
+
+			string s = this.name;
+			int markerNumber = Convert.ToInt32(s[s.Length - 1]);
+			print(markerNumber);
+			back.GetComponent<BackScript>().GameWinConditionCheck(markerNumber, 1);
+		}
+	}
+}
